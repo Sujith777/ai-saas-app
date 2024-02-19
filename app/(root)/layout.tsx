9080e8502523e1c348225e9c92aeb55d99996c88
bspraveen17 <billasujith77@gmail.com>
2024-02-19 20:28:40 +0530

    Add sidebar to homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
     </main>
   );
-}
+};
+
+export default Layout;
