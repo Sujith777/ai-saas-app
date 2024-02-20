b0eff1da41ba51244ef14bb8344fb245a40bc0bc
bspraveen17 <billasujith77@gmail.com>
2024-02-20 23:09:44 +0530

    Correction in layout routes

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/shared/Header";
+
+const AddTransformationTypePage = () => {
+  return (
+    <Header title="Transformation Title" subtitle="Transformation Subtitle" />
+  );
+};
+
+export default AddTransformationTypePage;
